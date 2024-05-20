31242caf40f5eb7535159de07e0b436a83299ecb
Bappy Modok <121638025+Bappy00111@users.noreply.github.com>
2024-05-20 10:38:46 +0600

    search flight section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Flight from './Flight';
+import SearchFlight from './SearchFlight';
 
 const Home = () => {
     return (
         <div>
             <Flight/>
+            <SearchFlight/>
         </div>
     );
 };
---
+++ src/components/SearchFlight.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+const SearchFlight = () => {
+  return (
+    <div className="py-10 sm:max-w-xl md:max-w-full lg:max-w-screen-xl mx-auto">
+      <div className="text-center space-y-3">
+        <h1 className="font-semibold md:text-4xl text-xl">
+          Search Cheapest Flight By Destination
+        </h1>
+        <p className="text-slate-500">
+          Effortlessly find cheap flight and air tickets to various destinations
+          on ShareTrip. Explore <br /> routes, book online air tickets, and plan
+          your next travel adventure.
+        </p>
+        <div className="pt-5 ">
+          <button className="btn btn-info text-white mr-3">Domestic</button>
+          <button className="btn ">International</button>
+        </div>
+      </div>
+      <div className="grid  grid-col-2 md:grid-cols-3 gap-4 py-5">
+      <div className="borer border-2 p-3 flex gap-5 rounded-xl">
+         <div>
+            <p>Dhaka</p>
+            <p>Hazrat shahjalal international airport</p>
+         </div>
+        <div>
+            <p>Cox's Bazar</p>
+            <p>Cox's Bazar Airport</p>
+        </div>
+      </div>
+      <div className="borer border-2 p-3 flex gap-5 rounded-xl">
+         <div>
+            <p>Dhaka</p>
+            <p>Hazrat shahjalal international airport</p>
+         </div>
+        <div>
+            <p>Cox's Bazar</p>
+            <p>Cox's Bazar Airport</p>
+        </div>
+      </div>
+      <div className="borer border-2 p-3 flex gap-5 rounded-xl">
+         <div>
+            <p>Dhaka</p>
+            <p>Hazrat shahjalal international airport</p>
+         </div>
+        <div>
+            <p>Cox's Bazar</p>
+            <p>Cox's Bazar Airport</p>
+        </div>
+      </div>
+      <div className="borer border-2 p-3 flex gap-5 rounded-xl">
+         <div>
+            <p>Dhaka</p>
+            <p>Hazrat shahjalal international airport</p>
+         </div>
+        <div>
+            <p>Cox's Bazar</p>
+            <p>Cox's Bazar Airport</p>
+        </div>
+      </div>
+      <div className="borer border-2 p-3 flex gap-5 rounded-xl">
+         <div>
+            <p>Dhaka</p>
+            <p>Hazrat shahjalal international airport</p>
+         </div>
+        <div>
+            <p>Cox's Bazar</p>
+            <p>Cox's Bazar Airport</p>
+        </div>
+      </div>
+      <div className="borer border-2 p-3 flex gap-5 rounded-xl">
+         <div>
+            <p>Dhaka</p>
+            <p>Hazrat shahjalal international airport</p>
+         </div>
+        <div>
+            <p>Cox's Bazar</p>
+            <p>Cox's Bazar Airport</p>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchFlight;
